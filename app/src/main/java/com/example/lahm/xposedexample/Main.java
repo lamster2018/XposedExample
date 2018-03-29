852d581bd9a535c18e45580786a91e325d07bf64
lahm <l292538769@gmail.com>
2018-03-29 11:27:15 +0800

    fix bug

--- app/src/main/java/com/example/lahm/xposedexample/Main.java
+++ app/src/main/java/com/example/lahm/xposedexample/Main.java
@@ -17,6 +17,8 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
 
 /**
  * Project Name:XposedExample
@@ -28,48 +30,39 @@ public class Main implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (loadPackageParam.packageName.contains("com.tencent")) {
-            XposedBridge.log("fck--open----" + PreferencesUtils.isOpen());
-            if (PreferencesUtils.isOpen()) {
-//            hookBushuByHookALL(loadPackageParam);
-//            hookBushuByFindAndHook(loadPackageParam);
-//            hookMenu(loadPackageParam);
+            try {
+                XposedBridge.hookAllMethods(
+                        findClass("android.hardware.SystemSensorManager$SensorEventQueue",
+                                loadPackageParam.classLoader),
+                        "dispatchSensorEvent",
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                super.beforeHookedMethod(param);
+                                int intValue = ((Integer) param.args[0]).intValue();
+                                Field declaredField = param.thisObject.getClass().getDeclaredField("mSensorsEvents");
+                                declaredField.setAccessible(true);
+                                Sensor sensor = ((SensorEvent)
+                                        ((SparseArray) declaredField.get(param.thisObject)).get(intValue)).sensor;
+                                if (sensor == null) {
+                                    XposedBridge.log("fucku---传感器为NULL");
+                                    return;
+                                }
+                                int sensorType = sensor.getType();
+                                if (sensorType == 19 || sensorType == 18) {
+                                    //500倍差不多了
+                                    float step = ((float[]) param.args[1])[0] * 101;
+                                    XposedBridge.log("fucku---" + loadPackageParam.packageName
+                                            + " 传感器类型 " + sensorType
+                                            + " 当前步数 " + ((float[]) param.args[1])[0]
+                                            + " 修改 " + step);
+                                    ((float[]) param.args[1])[0] = step;
+                                }
+                            }
+                        });
+            } catch (Exception e) {
+                XposedBridge.log("fucku--caonimashabi");
             }
-        }
-    }
-
-    private void hookBushuByHookALL(final XC_LoadPackage.LoadPackageParam loadPackageParam) {
-        try {
-            XposedBridge.hookAllMethods(
-                    XposedHelpers.findClass("android.hardware.SystemSensorManager$SensorEventQueue",
-                            loadPackageParam.classLoader),
-                    "dispatchSensorEvent",
-                    new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            super.beforeHookedMethod(param);
-                            int intValue = ((Integer) param.args[0]).intValue();
-                            Field declaredField = param.thisObject.getClass().getDeclaredField("mSensorsEvents");
-                            declaredField.setAccessible(true);
-                            Sensor sensor = ((SensorEvent)
-                                    ((SparseArray) declaredField.get(param.thisObject)).get(intValue)).sensor;
-                            if (sensor == null) {
-                                XposedBridge.log("fck---传感器为NULL");
-                                return;
-                            }
-                            int sensorType = sensor.getType();
-                            if (sensorType == 19 || sensorType == 18) {
-                                //500倍差不多了
-                                float step = ((float[]) param.args[1])[0] * PreferencesUtils.getRatio();
-                                XposedBridge.log("fck---" + loadPackageParam.packageName
-                                        + " 当前步数 " + ((float[]) param.args[1])[0]
-                                        + " 倍数 " + PreferencesUtils.getRatio()
-                                        + " 修改 " + step);
-                                ((float[]) param.args[1])[0] = step;
-                            }
-                        }
-                    });
-        } catch (Exception e) {
-            XposedBridge.log("fck--caonimashabi");
         }
     }
 
